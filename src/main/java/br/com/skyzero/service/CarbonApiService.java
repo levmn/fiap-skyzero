725a68487137a9b2e09d4dc96004ebf1a811992f
levmn <levimncontato@gmail.com>
2024-11-18 13:17:21 -0300

    Adicionando camada service

---
+++ src/main/java/br/com/skyzero/service/CarbonApiService.java
@@ -0,0 +1,40 @@
+package br.com.skyzero.service;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+
+public class CarbonApiService {
+    private static final String API_URL = "https://api-calculators.carbonext.com.br/v2/calculators/distance";
+
+    public double calcularEmissao(String tipoAviao, double distancia) {
+        Client client = ClientBuilder.newClient();
+
+        JsonObject jsonRequest = Json.createObjectBuilder()
+                .add("distance", distancia)
+                .add("type", tipoAviao)
+                .build();
+
+        Response response = client.target(API_URL)
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(jsonRequest));
+
+        try {
+            if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+                throw new RuntimeException("Erro na API: " + response.getStatus() + " - " + response.getStatusInfo().getReasonPhrase());
+            }
+
+            JsonObject jsonResponse = response.readEntity(JsonObject.class);
+            return jsonResponse.getJsonNumber("emission").doubleValue();
+
+        } finally {
+            response.close();
+            client.close();
+        }
+    }
+}
--- src/main/java/br/com/skyzero/utils/skyzero-script.sql
+++ src/main/java/br/com/skyzero/utils/skyzero-script.sql
@@ -1,7 +1,7 @@
 -- Drops
-DROP TABLE tb_usuario;
-DROP TABLE tb_login;
-DROP TABLE tb_registro;
+DROP TABLE tb_usuario CASCADE CONSTRAINTS;
+DROP TABLE tb_login CASCADE CONSTRAINTS;
+DROP TABLE tb_registro CASCADE CONSTRAINTS;
 
 DROP SEQUENCE tb_usuario_id_usuario_seq;
 DROP SEQUENCE tb_registro_id_registro_seq;
@@ -14,7 +14,7 @@ CREATE TABLE tb_usuario (
     cnpj VARCHAR2(14) UNIQUE NOT NULL
 );
 
-ALTER TABLE tb_usuario ADD CONSTRAINT tb_usuario_id_usuario_pk PRIMARY KEY ( id_usuario );
+ALTER TABLE tb_usuario ADD CONSTRAINT tb_usuario_id_usuario_pk PRIMARY KEY (id_usuario);
 
 -- Tabela para armazenar os logins (associados aos usuários)
 CREATE TABLE tb_login (
@@ -23,11 +23,11 @@ CREATE TABLE tb_login (
     senha VARCHAR2(255) NOT NULL
 );
 
-ALTER TABLE tb_login ADD CONSTRAINT tb_login_id_usuario_pk PRIMARY KEY ( id_usuario );
+ALTER TABLE tb_login ADD CONSTRAINT tb_login_id_usuario_pk PRIMARY KEY (id_usuario);
 
 ALTER TABLE tb_login
-    ADD CONSTRAINT tb_login_id_usuario_fk FOREIGN KEY ( id_usuario )
-        REFERENCES tb_usuario ( id_usuario );
+    ADD CONSTRAINT tb_login_id_usuario_fk FOREIGN KEY (id_usuario)
+        REFERENCES tb_usuario (id_usuario);
 
 -- Tabela para armazenar os registros (cálculos e histórico)
 CREATE TABLE tb_registro (
@@ -46,7 +46,6 @@ ALTER TABLE tb_registro
         REFERENCES tb_usuario (id_usuario);
 
 -- Sequences
-
 CREATE SEQUENCE tb_usuario_id_usuario_seq START WITH 1 NOCACHE ORDER;
 
 CREATE OR REPLACE TRIGGER tb_usuario_id_usuario_trg BEFORE
